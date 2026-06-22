fda3859b35252b642ade2f8f68243b13b1de0219
Kirana Fitria <krnfitria06@gmail.com>
2026-06-22 14:57:45 +0700

    "Splash Screen"

---
+++ app/src/main/java/com/kevin/anilist/SplashActivity.java
@@ -0,0 +1,23 @@
+package com.kevin.anilist;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private static final long SPLASH_DELAY_MS = 2500;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler(Looper.getMainLooper()).postDelayed(() -> {
+            startActivity(new Intent(SplashActivity.this, MainActivity.class));
+            finish();
+        }, SPLASH_DELAY_MS);
+    }
+}
